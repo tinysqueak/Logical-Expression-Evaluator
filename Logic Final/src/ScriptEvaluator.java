e67c8ed97ec596b02cea4f3e5a2700c4cb3b3345
William Yang <tinysqueak@gmail.com>
2016-08-17 02:10:04 -0700

    Added ScriptEvaluator

---
+++ Logic Final/src/ScriptEvaluator.java
@@ -0,0 +1,24 @@
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+public class ScriptEvaluator {
+
+	public static void main(String[] args) {
+
+		String boolexpr1 = "(!(T||false)&&!((true)^(true)))";
+		String boolexpr2 = "false||true&&!false";
+
+		boolean result = false;
+		
+		try {
+			result = (Boolean) new ScriptEngineManager().getEngineByName("javascript").eval(boolexpr2);
+		} catch (ScriptException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		System.out.println(result);
+
+	}
+
+}
